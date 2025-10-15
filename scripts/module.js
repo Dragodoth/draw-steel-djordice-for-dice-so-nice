c1a07890262d4d43cbd1b5b81deb25646dcf2828
Viktor <129966609+Dragodoth@users.noreply.github.com>
2025-10-15 15:19:02 +0200

    initial commit

--- scripts/module.js
+++ scripts/module.js
@@ -1,7 +1,14 @@
-Hooks.once('init', async function() {
-
-});
-
-Hooks.once('ready', async function() {
-
-});
+Hooks.on('diceSoNiceReady', (dice3d) => {
+    dice3d.addSystem({ id: 'djordice', name: '⚔️ Djordice' }, 'default');
+    
+    const diceobj = dice3d.DiceFactory.get('d10')
+    diceobj.mass = 500
+    diceobj.system = 'djordice'
+    diceobj.shape = 'd20'
+    diceobj.inertia = 9
+    diceobj.labels = ['1','2','3','4','5','6','7','8','9','10','10','9','8','7','6','5','4','3','2','1']
+    diceobj.values = [1,2,3,4,5,6,7,8,9,10,10,9,8,7,6,5,4,3,2,1]
+    diceobj.scale = 1
+    diceobj.diceSystem = dice3d.DiceFactory.systems.get('djordice')
+    console.log(diceobj)
+})
