495548440af5a67399b3c5e486e512fb453508f3
Viktor <129966609+Dragodoth@users.noreply.github.com>
2025-10-17 18:53:57 +0200

    Update module.js

--- scripts/module.js
+++ scripts/module.js
@@ -1,13 +1,9 @@
 Hooks.on('diceSoNiceReady', (dice3d) => {
-    dice3d.addSystem({ id: 'djordice', name: '⚔️ Djordice' }, 'default');
-    
     const diceobj = dice3d.DiceFactory.get('d10')
     diceobj.mass = 500
-    diceobj.system = 'djordice'
     diceobj.shape = 'd20'
     diceobj.inertia = 9
-    diceobj.labels = ['1','2','3','4','5','6','7','8','9','10','10','9','8','7','6','5','4','3','2','1']
-    diceobj.values = [1,2,3,4,5,6,7,8,9,10,10,9,8,7,6,5,4,3,2,1]
+    diceobj.labels = ['1','4','7','8','5','3','6','9','10','2','2','10','9','6','3','5','8','7','4','1']
+    diceobj.values = [1,4,7,8,5,3,6,9,10,2,2,10,9,6,3,5,8,7,4,1]
     diceobj.scale = 1
-    diceobj.diceSystem = dice3d.DiceFactory.systems.get('djordice')
 })
